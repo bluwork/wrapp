ad7c622615f4c87cbfbb3b2883fd38e1a8c5bee1
BLu <bbnlkc@gmail.com>
2022-09-30 18:40:36 +0200

    Put helmet on app

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import helmet from 'helmet';
 import routes from './routes';
 import errorHandler from './middleware/error/error-handler';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(helmet());
 
 app.use('/', routes);
 
