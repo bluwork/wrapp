deb6ca68408c7f7f1aee5f475f4de6f2fd0af9d0
BLu <bbnlkc@gmail.com>
2022-09-30 23:59:19 +0200

    Add basic app test

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,12 @@
-import { Router } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 import extendRequestWithBundle from '../middleware/extensions/extend-request';
 import reportRouter from './report-router';
 
 const router = Router();
+
+router.get('/', (req: Request, res: Response) => {
+  res.json({ message: 'Hello from Wrapi!' });
+});
 
 router.use('/report', extendRequestWithBundle, reportRouter);
 
